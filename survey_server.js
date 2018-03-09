78e448fe9e651832e6b6788a44c7b15d0235ecfc
Fanyo Siliadin <fanyo.siliadin.prestataire@sfr.com>
2018-03-09 11:41:37 +0100

    move form on https server

---
+++ survey_server.js
@@ -0,0 +1,41 @@
+var https = require('https')
+var fs = require('fs')
+
+var whiteList = ['css/', 'dependencies/', 'scripts/', 'index.html', 'checkMandatoryQuestions.js', 'survey.css']
+var options = {
+	key: fs.readFileSync('self-signedCertificate/key.pem'),
+	cert: fs.readFileSync('self-signedCertificate/cert.pem')
+}
+
+https.createServer(options, function(request, response) {
+	request.url += request.url === '/' ? 'index.html' : ''
+	
+	if (isAuthorized(request.url)) {		
+		fs.readFile('.'+request.url, function(err, file) {
+			response.writeHead(200, {
+				'Content-Type': getType(request.url)
+	      	})
+			response.end(file)
+		})
+	} else {
+		console.log('UNAUTHORIZED REQUEST')
+		response.writeHead(404, {
+      	})
+      	response.end('Page not found')
+	}
+	
+	
+	
+}).listen(process.env.PORT || 8086)
+
+function getType (url) {
+	return 'text/'+url.split('.').pop()
+} 
+
+function isAuthorized (url) {
+	return whiteList.some(function(folder){
+		return url.indexOf(folder) === 1
+	})
+}
+
+console.log('ui server running at 127.0.0.1:' + (process.env.PORT || 8086))
