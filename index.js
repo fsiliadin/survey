b21357b5f4cb730da504c979c99c19498f566051
Fanyo <fsiliadin@gmail.com>
2018-06-18 14:22:36 +0200

    allow request from whitelist

--- index.js
+++ index.js
@@ -1,54 +1,62 @@
 var https = require('http')
 var fs = require('fs')
 
-var whiteList = ['focus.js', 'focus.css', 'index.html', 'survey_script.js', 'survey.css']
+var whiteList = ['focus.js', 'focus.css', 'index.html', 'survey_script.js', 'survey.css', 'Roboto-Regular.ttf', 'Roboto-Light.ttf']
 
 https.createServer(function(request, response) {
 	console.log('METHOD', request.method)
 	response.setHeader('Access-Control-Allow-Origin', '*')
-	if (request.method === 'POST') {
-		var data = '';
-		request.on('data', function (chunk) {
-			data += chunk;
-		});
-		request.on('end', function () {
-			data = JSON.parse(data)
-			var content;
-			fs.readFile('surveyResponses.json', function(err, file) {
-				responses = JSON.parse(file.toString())
-				responses.push(data)
-				content = JSON.stringify(responses)
-				console.log('CONTENT', content)
-				fs.writeFile('surveyResponses.json', content, function(err) {
-					if(err){
-						console.log('an error occured')
-					} else {
-						console.log('DATA UPDATED')
-					}
+
+	if (isOriginAllowed(request.headers.origin) || request.url === '/') {
+		if (request.method === 'POST') {
+			var data = '';
+			request.on('data', function (chunk) {
+				data += chunk;
+			});
+			request.on('end', function () {
+				data = JSON.parse(data)
+				var content;
+				fs.readFile('surveyResponses.json', function(err, file) {
+					responses = JSON.parse(file.toString())
+					responses.push(data)
+					content = JSON.stringify(responses)
+					console.log('CONTENT', content)
+					fs.writeFile('surveyResponses.json', content, function(err) {
+						if(err){
+							console.log('an error occured')
+						} else {
+							console.log('DATA UPDATED')
+						}
+					})
 				})
-			})
-			
-			// save data in DB
-			response.end('thank you :)')
-			
-		});
-	} else {
-		request.url += request.url === '/' ? 'index.html' : ''
-		
-		if (isAuthorized(request.url)) {		
-			fs.readFile('.'+request.url, function(err, file) {
-				response.writeHead(200, {
-					'Content-Type': getType(request.url)
-		      	})
-				response.end(file)
-			})
+				
+				// save data in DB
+				response.end('thank you :)')
+				
+			});
 		} else {
-			console.log('UNAUTHORIZED REQUEST')
-			response.writeHead(404, {
-	      	})
-	      	response.end('Page not found')
+			request.url += request.url === '/' ? 'index.html' : ''
+			
+			if (isAuthorized(request.url)) {		
+				fs.readFile('.'+request.url, function(err, file) {
+					response.writeHead(200, {
+						'Content-Type': getType(request.url)
+			      	})
+					response.end(file)
+				})
+			} else {
+				console.log('UNAUTHORIZED REQUEST')
+				response.writeHead(404, {
+		      	})
+		      	response.end('Page not found')
+			}
 		}
+	} else {
+		response.writeHead(404, {
+	      	})
+      	response.end('fuck you')
 	}
+	
 	
 	
 	
@@ -60,9 +68,14 @@ function getType (url) {
 } 
 
 function isAuthorized (url) {
-	return whiteList.some(function(folder){
-		return url.indexOf(folder) === 1
+	return whiteList.some(function(file){
+		return url.indexOf(file) === 1
 	})
+}
+
+function isOriginAllowed(origin) {
+	console.log('origin', origin)
+	return origin === 'https://clubfinderform.pw' || origin === 'https://clubfinderformpro.pw'
 }
 
 console.log('ui server running at 127.0.0.1:' + (process.env.PORT || 8086))
